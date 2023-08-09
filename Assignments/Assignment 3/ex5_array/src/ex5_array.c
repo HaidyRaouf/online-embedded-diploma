aa2be2dab3bc6f256d70d5b2d4a3dbeedbf669b6
Haidy Raouf <85240515+HaidyRaouf@users.noreply.github.com>
2023-08-09 20:46:07 +0300

    editing the soln for ex5_array

--- Assignments/Assignment 3/ex5_array/src/ex5_array.c
+++ Assignments/Assignment 3/ex5_array/src/ex5_array.c
@@ -12,26 +12,31 @@
 #include <stdlib.h>
 
 int main(void) {
-	int n, i, e, l;
-		int arr[100];
-		printf("enter no of elements: \n");
-		fflush(stdout);
-		scanf("%d", &n);
-		for (i=0; i<n; i++)
+	int n, i, e, l, flag=0;
+	int arr[100];
+	printf("enter no of elements: \n");
+	fflush(stdout);
+	scanf("%d", &n);
+	for (i=0; i<n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	printf("enter the element to be searched:\n ");
+	fflush(stdout);
+	scanf("%d", &e);
+	for (i=0; i<n; i++)
+	{
+		if (arr[i]==e)
 		{
-			scanf("%d", &arr[i]);
+			l=i;
+			flag=1;
+			break;
 		}
-		printf("enter the element to be searched:\n ");
-		fflush(stdout);
-		scanf("%d", &e);
-		for (i=0; i<n; i++)
-		{
-			if (arr[i]==e)
-			{
-				l=i;
-			}
-			else
-				continue;
-		}
+		else
+			continue;
+	}
+	if (flag ==1)
 		printf("number found at the location= %d \n", l+1);
+	else
+		printf("number not found");
 }
