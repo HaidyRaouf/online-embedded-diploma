d8ca43aab5058fea3169c9ba67c9eebe7b20a860
Haidy Raouf <85240515+HaidyRaouf@users.noreply.github.com>
2023-08-09 20:41:46 +0300

    adding ex 5 soln

---
+++ Assignments/Assignment 3/ex5_array/src/ex5_array.c
@@ -0,0 +1,37 @@
+/*
+ ============================================================================
+ Name        : ex5_array.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int n, i, e, l;
+		int arr[100];
+		printf("enter no of elements: \n");
+		fflush(stdout);
+		scanf("%d", &n);
+		for (i=0; i<n; i++)
+		{
+			scanf("%d", &arr[i]);
+		}
+		printf("enter the element to be searched:\n ");
+		fflush(stdout);
+		scanf("%d", &e);
+		for (i=0; i<n; i++)
+		{
+			if (arr[i]==e)
+			{
+				l=i;
+			}
+			else
+				continue;
+		}
+		printf("number found at the location= %d \n", l+1);
+}
